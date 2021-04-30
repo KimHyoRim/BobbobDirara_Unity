869249188fa0ffd6f2680311e4a99f73977ca3c0
gxg1119 <70965350+gxg1119@users.noreply.github.com>
2021-04-30 12:26:35 +0900

    커밋 1
    이그노어 설정 후 만들어 오던 것 커밋

---
+++ Assets/Player_move.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_move : MonoBehaviour
+{
+    public Rigidbody player_rigid;
+    public float movementspeed = 10.0f;
+    void Update()
+    {
+        // 마우스 입력
+        var mouseX = Input.GetAxis("Mouse X");
+        var mouseY = Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = transform.eulerAngles + new Vector3(x: 0, y: mouseX, z: 0);
+        
+        // 키보드 입력
+        var keyX = Input.GetAxis("Horizontal");
+        var keyZ = Input.GetAxis("Vertical");
+
+        player_rigid.velocity = transform.forward * (keyZ * movementspeed) + transform.right * (keyX * movementspeed);
+    }
+}
