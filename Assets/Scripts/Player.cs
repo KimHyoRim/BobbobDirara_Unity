c9feaca3e08a27a71490256a9f1f07644eaa6ba8
gxg1119 <70965350+gxg1119@users.noreply.github.com>
2021-05-06 23:06:05 +0900

    카메라, 캐릭터 이동 수정 중

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,6 +12,25 @@ public class Player : MonoBehaviour
 
     Animator anim;
 
+    [SerializeField]
+    private float lookSensitivity;
+
+    [SerializeField]
+    private float cameraRotationLimit; //최대 카메라 회전각 75정도면 적당
+
+    // 캐릭터 회전 변수
+    private float currentCharactorRotationX;
+    private float currentCharactorRotationY;
+
+    // 카메라 회전 변수
+    private float currentCameraRotationX;
+    private float currentCameraRotationY;
+
+    [SerializeField]
+    private Camera theCamera;
+    private Rigidbody myRigid;
+
+
     void Awake()
     {
         anim = GetComponent<Animator>();
@@ -19,10 +38,20 @@ public class Player : MonoBehaviour
     }
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        myRigid = GetComponent<Rigidbody>();
     }
 
     void Update()
+    {
+        Move();
+        CharactorRotation();
+        CameraRotation();
+    }
+
+    void Move()
     {
         hAxis = Input.GetAxisRaw("Horizontal");
         vAxis = Input.GetAxisRaw("Vertical");
@@ -32,7 +61,41 @@ public class Player : MonoBehaviour
         transform.position += moveVec * speed * Time.deltaTime;
 
         anim.SetBool("isWalk", moveVec != Vector3.zero);
+        //transform.LookAt(transform.position + moveVec);
+    }
 
-        transform.LookAt(transform.position + moveVec);
+    void CameraRotation()
+    {
+        float yRotation = Input.GetAxisRaw("Mouse X");
+        //float cameraRaotationY = yRotation * lookSensitivity;
+
+        float xRotation = Input.GetAxisRaw("Mouse Y");
+        //float cameraRaotationX = xRotation * lookSensitivity;
+
+        theCamera.transform.eulerAngles = transform.eulerAngles + new Vector3(x: 0, y: yRotation, z: 0);
+        //currentCameraRotationX -= cameraRaotationX;
+        //currentCameraRotationY -= cameraRaotationY;
+
+        //currentCameraRotationX = Mathf.Clamp(currentCameraRotationX, -cameraRotationLimit, cameraRotationLimit);
+
+        //theCamera.transform.localEulerAngles = new Vector3(currentCameraRotationX, -currentCameraRotationY, 0f);
+    }
+
+    void CharactorRotation()
+    {
+        float char_Y_Ro = Input.GetAxisRaw("Mouse X");
+        //float CharactoRaotationY = char_Y_Ro * lookSensitivity;
+
+        float char_X_Ro = Input.GetAxisRaw("Mouse Y");
+
+        transform.eulerAngles = transform.eulerAngles + new Vector3(x: 0, y: char_Y_Ro, z: 0);
+        //float CharactoRaotationX = char_X_Ro * lookSensitivity;
+
+        //currentCharactorRotationX -= CharactoRaotationX;
+        //currentCharactorRotationY -= CharactoRaotationY;
+
+        //currentCharactorRotationX = Mathf.Clamp(currentCharactorRotationX, -cameraRotationLimit, cameraRotationLimit);
+
+        //transform.localEulerAngles = new Vector3(0f, -currentCharactorRotationY, 0f);
     }
 }
